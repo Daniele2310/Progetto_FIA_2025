cb77899d467f95402810e77778544e487f4f18d6
Daniele Cantagallo <danielecantagallo1@gmail.com>
2025-12-26 15:23:52 +0100

    Implementata la prima parte del main, con la parte di import e data cleaning, la divisione dei tre metodi (sempre con l'ausilio del parser) e l'inizio della scrittura del knn

--- main.py
+++ main.py
@@ -45,3 +45,68 @@ def parse_args():
                         help="Seed casuale per la riproducibilità")
 
     return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    print("--- Configurazione ---")
+    print(f"File: {args.file}")
+    print(f"Metodo: {args.method}")
+    print(f"K-NN Neighbors: {args.k_nn}")
+    print(f"Seed: {args.seed}")
+    print("----------------------\n")
+
+    classes = "classtype_v1"
+    loader = Data_Loader(args.file, [], classes)
+    df = loader.load_dataset()
+    print(df)
+
+    if df is None:
+        print("Errore: Impossibile caricare il dataset")
+
+    columns = df.columns.tolist()
+    if classes in columns:
+        columns.remove(classes)
+
+    loader.features_names = columns
+
+    loader.features_cleaning_and_extraction()
+    X = loader.X
+    Y = loader.Y
+
+    if isinstance(Y, pd.DataFrame):
+        Y = Y.iloc[:,0]
+
+    splits = []
+
+    if args.method == 'holdout':
+        res = holdout(X, Y, test_size=args.test_size, random_state=args.seed)
+        if res:
+            splits.append(res)
+
+    elif args.method == 'subsampling':
+        splits = random_subsampling(X, Y, test_size=args.test_size, n=args.n_iter, random_state=args.seed)
+
+    elif args.method == 'bootstrap':
+        splits = bootstrap(X, Y, k=args.k_boot, random_state=args.seed)
+
+    if not splits:
+        print("Errore nella generazione degli split o interruzione utente.")
+
+    knn = KNN_Classifier(K = args.k_nn)
+    for i, (X_train, X_test, Y_train, Y_test) in enumerate(splits):
+        knn.fit(X_train, Y_train)
+        y_pred = knn.predict(X_test)
+        y_proba = knn.predict_proba(X_test)
+
+        evaluator = MetricsEvaluator(Y_test, y_pred, Y_scores=y_proba, pos_label=4)
+        metrics = evaluator.get_metrics()
+        print(metrics)
+        evaluator.plot_confusion_matrix()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
